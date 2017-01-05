c885e78c7207f7057535006990c6b99b8174b7d2
Albert Yanit <albertyanit@albertyanitsmbp.home>
2017-01-05 15:06:35 +0000

    Complete exercises 1 and 2

---
+++ chap01/ex1.rb
@@ -0,0 +1,9 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Another line added to my script"
+# puts "The # symbol is used for writing notes and this code line will be ignored." 

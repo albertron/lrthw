8709f0a474cd6848058e170f2e6c7a1677a0e418
Albert Yanit <albertyanit@albertyanitsmbp.home>
2017-01-06 11:01:38 +0000

    Completed ex 6

--- chap01/ex4.rb
+++ chap01/ex4.rb
@@ -1,21 +1,21 @@
-# Assigning the variable "cars" which holds the value of 100 (Integer)
+# Assigning the variable cars which holds the value of 100 (Integer)
 cars = 100
-# Assigning the variable "space_in_a_car" which holds the value of 4.0 (Float)
+# Assigning the variable space_in_a_car which holds the value of 4.0 (Float)
 space_in_a_car = 4.0
-# Assigning the variable "drivers" which holds the value of 30 (Integer)
+# Assigning the variable drivers which holds the value of 30 (Integer)
 drivers = 30
-# Assigning the variable "passengers" which holds the value of 90 (Integer)
+# Assigning the variable passengers which holds the value of 90 (Integer)
 passengers = 90
-# Assigning the variable "cars_not_driven" which equates to cars (currently assigned to 100) minus
+# Assigning the variable cars_not_driven which equates to cars (currently assigned to 100) minus
 # drivers (currently assigned to 30)
 cars_not_driven = cars - drivers
-# Assigning the variable of "cars_driven" to another variable "drivers"
+# Assigning the variable of cars_driven to another variable drivers
 cars_driven = drivers
-# Assining the variable of "carpool_capacity" which equates to variable "cars_driven" multiplied by variable 
-# "space_in_a_car"
+# Assining the variable of carpool_capacity which equates to variable cars_driven multiplied by variable 
+# space_in_a_car
 carpool_capacity = cars_driven * space_in_a_car
-# Assigning the variable of "average_passengers_per_car" which equates to variable "passengers" 
-# divided by the variable "cars_driven"
+# Assigning the variable of average_passengers_per_car which equates to variable passengers
+# divided by the variable cars_driven
 average_passengers_per_car = passengers / cars_driven
 
 # Prints strings which summarizes cars available, drivers available, how many empty cars, how many people can be 
@@ -33,4 +33,4 @@ puts "We need to put about #{averge_passengers_per_car} in each car."
 # On the current file, at line 14, there is a name error. The variable "carpool_capacity" has not been 
 # defined and to fix this simply assign it.
 
-# Variable "space_in_a_car" should be an integer rather than a float as you can't have half of a space in a car.
+# Variable space_in_a_car should be an integer rather than a float as you can't have half of a space in a car.
---
+++ chap01/ex6.rb
@@ -0,0 +1,33 @@
+# Variable types_of_people holds the value of 10
+types_of_people = 10
+# x is assigned to a string which states how many types of people
+x = "There are #{types_of_people} types of people."
+# Variable binary is assigned to the string "binary"
+binary = "binary"
+# Variable do_not is assigned to the string "don't"
+do_not = "don't"
+# Variable y is assigned to a string which makes a statement 
+y = "Those who know #{binary} and those who #{do_not}."
+
+# Prints variable x and y
+puts x
+puts y
+
+# Prints string which includes x and y
+puts "I said: #{x}."
+puts "I also said: '#{y}'."
+
+# Variable hilarious holds the boolean value of false
+hilarious = false
+# Variable joke_evaluation is assigned to a string which uses variable hilarious
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
+
+# Prints variable joke_evaluation
+puts joke_evaluation
+ 
+# Assigns variables w and e to a string
+w = "This is the left side of..."
+e = "a string with a right side."
+
+# Prints out variable w added with e
+puts w + e

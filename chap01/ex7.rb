5d6878d8249aa4e510b437711e6aa18748395856
Albert Yanit <albertyanit@albertyanitsmbp.home>
2017-01-06 14:10:59 +0000

    Completed exercise 7

---
+++ chap01/ex7.rb
@@ -0,0 +1,26 @@
+# Prints following strings ...
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+# Prints string "." ten times.
+puts "." * 10 # what'd that do?
+
+# Assigns different variables to different strings
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# wact that print vs. puts on this line what's it do?
+# Print allows code to continue on same line rather than start a new line.
+# Prints "CheeseBurger" onto one line
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12  

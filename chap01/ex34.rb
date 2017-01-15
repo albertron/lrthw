685afae500ba9cf4439c062481d1fe816ea573c0
Albert Yanit <albertyanit@albertyanitsmbp.home>
2017-01-15 20:23:34 +0000

    Completed exercise 34

---
+++ chap01/ex34.rb
@@ -0,0 +1,36 @@
+ordinal == ordered # minus one
+cardinal == cards # direct
+
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+1. The animal at 1 - ruby 
+The animal at 1 is the 2nd animal and is a ruby
+The 2nd animal is at 1 and is a ruby
+
+2. The third (3rd)animal - peacock
+The 3rd animal is at 2 and is a peacock
+The animal at 2 is the 3rd animal and is a peacock
+
+3. The first (1st)animal - bear
+The 1st animal is at 0 and is a bear
+The animal at 0 is the 1st animal and is a bear
+
+4. The animal at 3 - kangaroo
+Th animal at 3 is the 4th animal and is a kangaroo
+The 4th animal is at 3 and is a kangaroo
+
+5. The fifth (5th)animal - whale
+The 5th animal is at 4 and is a whale
+The animal at 4 is the 5th animal and is a whale
+
+6. The animal at 2 - peacock
+The animal at 2 is the 3rd animal and is a peacock
+The 3rd animal is at 2 and is a peacock
+
+7. The sixth (6th)animal - platypus
+The sixth animal is at 5 and is a platypus
+The animal at 5 is the 6th animal and is a platypus
+
+8. The animal at 4 - whale
+The animal at 4 is the 5th animal and is a whale
+The fifth animal is at 4 and is a whale
